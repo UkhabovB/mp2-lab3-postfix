281bfc28a0ee0fa74fb8ceed5715b28f5f881e39
UkhabovB <ukhabovb1997@yandex.ru>
2017-02-14 14:39:47 +0300

    Update test_tpostfix.cpp

--- Сысоев/base_test/test_tpostfix.cpp
+++ Сысоев/base_test/test_tpostfix.cpp
@@ -1,6 +1,29 @@
 #include "postfix.h"
 #include <gtest.h>
 
+class TPostfixTest : public ::testing::Test
+{
+public:
+	TPostfix *pf;
+	TPostfixTest() : pf(NULL) {};
+    void CreatePostfix(string s)
+	{
+		pf = new TPostfix(s);
+	}
+	
+	~TPostfixTest()
+	{
+		delete pf;
+	}
+
+};
+TEST_F(TPostfixTest,can_create_postfix)
+{
+	ASSERT_NO_THROW(TPostfix pf("a+b"));
+}
+#include "postfix.h"
+#include <gtest.h>
+
 TEST(TPostfix, can_create_postfix)
 {
   ASSERT_NO_THROW(TPostfix p);
